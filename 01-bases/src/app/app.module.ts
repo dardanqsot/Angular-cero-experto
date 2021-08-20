9d08dd9723348bd0719914215f9540520b7c075e
Dardan <quispesotodaniel@gmail.com>
2021-08-20 00:58:43 -0500

    Finalización del primer módulo- Introducción a Angular

--- 01-bases/src/app/app.module.ts
+++ 01-bases/src/app/app.module.ts
@@ -2,17 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador/contador.component';
-import { HeroeComponent } from './heroes/heroe/heroe.component';
+import { HeroesModule } from './heroes/heroes.module';
+import { ContadorModule } from './contador/contador.module';
 
 @NgModule({
   declarations: [ /// para colocar componentes
-    AppComponent,
-    ContadorComponent,
-    HeroeComponent
+    AppComponent
   ],
   imports: [  /// para utilizar otros modulos
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],  //servicios especificos a un module
   bootstrap: [AppComponent] // Componente principal que se lanzara primero
---
+++ 01-bases/src/app/heroes/heroes.module.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { HeroeComponent } from './heroe/heroe.component';
+import { ListadoComponent } from './listado/listado.component';
+
+@NgModule({
+    declarations: [   /// aqui van los componentes que seran utilizados
+        HeroeComponent,
+        ListadoComponent
+    ],
+    exports:[ // los componentes o cosas que deseo usar fuera de este módulo
+        ListadoComponent
+    ],
+    imports:[ //Modulos de Angular a usar
+        CommonModule
+    ]
+
+})
+export class HeroesModule {
+
+}
