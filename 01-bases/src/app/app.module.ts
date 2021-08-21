3b71a78a1ad9069763d1dd180f909f2752e28042
Dardan <quispesotodaniel@gmail.com>
2021-08-21 10:34:51 -0500

    Creacion de componente  - export de componentes e importacion de  modulos

--- 01-bases/src/app/app.module.ts
+++ 01-bases/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeroesModule } from './heroes/heroes.module';
 import { ContadorModule } from './contador/contador.module';
+import { MainPageComponent } from './dbz/main-page/main-page.component';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [ /// para colocar componentes
@@ -12,7 +14,8 @@ import { ContadorModule } from './contador/contador.module';
   imports: [  /// para utilizar otros modulos
     BrowserModule,
     HeroesModule,
-    ContadorModule
+    ContadorModule,
+    DbzModule
   ],
   providers: [],  //servicios especificos a un module
   bootstrap: [AppComponent] // Componente principal que se lanzara primero
