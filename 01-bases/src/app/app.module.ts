2f5b1795dfa97c6c995350c08d4fb8cbe7b15755
Dardan <quispesotodaniel@gmail.com>
2021-08-22 19:05:26 -0500

    Utilizando forms Modulo para evitar recarga de pagina

--- 01-bases/src/app/app.module.ts
+++ 01-bases/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeroesModule } from './heroes/heroes.module';
 import { ContadorModule } from './contador/contador.module';
-import { MainPageComponent } from './dbz/main-page/main-page.component';
 import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
