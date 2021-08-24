b96e95de8435916f64da3187077306ab5a522fb1
Dardan <quispesotodaniel@gmail.com>
2021-08-23 23:05:28 -0500

    Uso de ngmodel

--- 01-bases/src/app/dbz/main-page/main-page.component.ts
+++ 01-bases/src/app/dbz/main-page/main-page.component.ts
@@ -1,14 +1,42 @@
 import { Component} from '@angular/core';
 
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html'
 })
 export class MainPageComponent {
 
+  personajes: Personaje[] = [
+    {
+      nombre: 'Goku',
+      poder: 15000
+    },
+    {
+      nombre: 'Vegeta',
+      poder: 10000
+    }
+  ];
+  
+  nuevo:Personaje = {
+    nombre: '',
+    poder: 0 
+  }
+
    agregar() {
      //event.preventDefault(); /// para prevenir recarga pero solo en javascript recibiendo $event como prametro con angular es mas rápido
-     
-     console.log("HEY")
+     if( this.nuevo.nombre.trim().length === 0) {
+       return;
+     }
+
+     this.personajes.push( this.nuevo );
+     this.nuevo = {
+      nombre: '',
+      poder: 0
+    }
    }
 }
